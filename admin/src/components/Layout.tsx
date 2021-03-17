a3c730d9dd2b2442be6d3b95e92b5b6737184f0f
Trevor Thalacker <trevor@trevorthalacker.me>
2021-03-16 17:21:15 -0700

    :sparkles: Screen size warning

--- admin/src/components/Layout.tsx
+++ admin/src/components/Layout.tsx
@@ -4,6 +4,15 @@ import { Header } from './Header'
 import { ThemeProvider } from '@material-ui/core'
 import { theme } from '../theme'
 
+const SizeWarning = () => {
+  return (
+    <div className={`size-warning`}>
+      <h1>Your screen is too small to use the Dot HQ Update Service!</h1>
+      <p>Please either resize your screen, or visit this page on a desktop machine.</p>
+    </div>
+  )
+}
+
 export const Layout = ({
   children,
   uData,
@@ -15,6 +24,7 @@ export const Layout = ({
 }) => {
   return (
     <ThemeProvider theme={theme}>
+      <SizeWarning />
       <section className={`hero`}>
         <Header uData={uData} isAuth={isAuth}/>
         {children}
